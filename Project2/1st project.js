ccd4cf12cd60866b5867ec5db50281542678fd11
AsthaVishwa <34264019+AsthaVishwa@users.noreply.github.com>
2018-03-13 20:07:28 +0530

    Projects

--- Project2/1st project.js
+++ Project2/1st project.js
@@ -10,3 +10,15 @@ function myfunction(){
 	}
 }
 
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
+
+// Navigation bar and introduction and images of underwater land 
+
+//signup page
+function function1(){
+window.alert("Your account is created");
+};
+
